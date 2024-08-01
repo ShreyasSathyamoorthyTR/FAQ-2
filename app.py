43c44e006b58608e1c41838dbc5e0d45e2251f6f
Sathyamoorthy <shreyas.sathyamoorthy@thomsonreuters.com>
2024-08-01 22:03:29 +0530

    Changed the prompt

--- app.py
+++ app.py
@@ -10,9 +10,7 @@ def main_page():
     cur.execute('SELECT area FROM area_new')
     areas_wrong_format=cur.fetchall()
     practice_areas=get_data_in_format_areas(areas_wrong_format)
-    dic={"practice_areas":practice_areas}
-    cache=(access_cache())
-    cache=[(dic)]
+    cache=[{}]
     update_cache(cache)
     close_connection(cur,conn)
     return render_template('index.html',areas=practice_areas)
@@ -22,10 +20,10 @@ def main_page():
 def subareas():
     area=request.form.get('main_area')
     cache=(access_cache())
-    cache[0]["selected_practice_areas"]=area
+    cache[0]["selected_practice_areas"]=capital_each(area)
     selected_sub_area=request.form.get('sub_area')
     question_count=request.form.get('question_count')
-    cache[0]["selected_sub_practice_areas"]=selected_sub_area
+    cache[0]["selected_sub_practice_areas"]=capital_each(selected_sub_area)
     cache[0]["question_count"]=str(question_count)
     cache[0]["prompt1"]=f"Please generate a list of 10 frequently asked questions and answers about {selected_sub_area} that falls under {area} in the law field. Each question should be formatted in an <h1> tag and each answer should be formatted in an <h2> tag. Ensure the questions and answers address various aspects relevant to clients seeking legal assistance, including how different practice areas and sub-practice areas of law firms manage these issues."
     if len((cache[0]["selected_sub_practice_areas"]).strip())<=2:
